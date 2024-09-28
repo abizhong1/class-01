cc92c841daaca2bb668eeb9e7fe7c5794c326db2
abizhong1 <abigailzhong219@gmail.com>
2024-09-28 19:53:30 -0400

    new test

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 const express = require('express');
 const app = express();
 exports.app = app;
-const mongoose = require('mongoose');
+const mongoose = require( 'mongoose');
 const User = require('./models/User');
 
-app.get('/', [], async (req, res) => {
-  const user = new User({
-    firstname: 'Max',
-    userName: 'Wang',
-  });
+app.get('/', [], async(req, res)=>{
+    const user= new User({
+        firstName:"Max",
+            userName:"Wang",
+    });
 
-  await user.save();
+         await user.save();
 
-  res.send('<h2>Hello from Express.js server!!!</h2>');
+    res.send('<h2>Hello from Express.js server!!!</h2>');
+    
 });
 
 mongoose
-  .connect(
-    'mongodb+srv://abigailzhong:admin@cluster0.131ai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    {
-      //  useNewUrlParser: true,
-      //  useUnifiedTopology: true,
-      dbName: 'class1-test',
-    },
-  )
-  .then(() => {
-    console.log('Database Connection is ready...');
-    app.listen(3000);
-    console.log('server listening on port 3000');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  .connect (
+   'mongodb+srv://abigailzhong:admin@cluster0.131ai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+   {
+    //  useNewUrlParser: true, 
+    //  useUnifiedTopology: true, 
+     dbName: 'class1-test',
+   }
+ )
+  .then ( () => {
+    console. log ('Database Connection is ready...');
+    app. listen (3000);
+    console. log('server listening on port 3000');
+})
+  .catch((err) =>{
+    console. log(err);
+});
